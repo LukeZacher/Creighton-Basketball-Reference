f143792904262b5041bf4701c347ec60a79e6add
nolansailors <122844276+nolansailors@users.noreply.github.com>
2024-11-25 12:02:29 -0600

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,37 @@
+function loadStats(file) {
+    fetch(file)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const tableBody = document.querySelector("#statsTable tbody");
+        tableBody.innerHTML = ""; // Clear the table body
+        data.players.forEach((player) => {
+          const row = document.createElement("tr");
+          row.innerHTML = `
+            <td>${player.name}</td>
+            <td>${player.PTS}</td>
+            <td>${player["PTS/G"]}</td>
+            <td>${player.FG_FGA}</td>
+            <td>${player["FG%"]}</td>
+            <td>${player["3FG_FGA"]}</td>
+            <td>${player["3PT%"]}</td>
+            <td>${player.FT_FTA}</td>
+            <td>${player["FT%"]}</td>
+            <td>${player.AST}</td>
+            <td>${player.STL}</td>
+            <td>${player.BLO}</td>
+            <td>${player.REB}</td>
+            
+          `;
+          tableBody.appendChild(row);
+        });
+      })
+      .catch((error) => {
+        console.error("Error loading stats:", error);
+      });
+  }
+  
